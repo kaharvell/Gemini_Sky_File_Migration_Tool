0e79481dac2cf017265b3b8bb1c7156d3fb21cee
kaharvell <62230854+kaharvell@users.noreply.github.com>
2020-06-26 15:44:38 -0600

    First Commit

---
+++ File_Migration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace GeminiSkyFileMigrationTool
+{
+    class FileMigration
+    {
+        static int moved = 0;
+        static int unableToMove = 0;
+        
+        public static string SourcePath { get; set; }
+        public static string DestinationPath { get; set; }
+        public static string Filter { get; set; }
+
+        public FileMigration(string sourcePath, string destinationPath, string filter)
+        {
+            SourcePath = sourcePath;
+            DestinationPath = destinationPath;
+            Filter = filter;
+        }
+
+        public static void ScanDirectory(string directory)
+        {
+            string[] fileExists = Directory.GetFiles(directory);
+            foreach (string file in fileExists)
+                ScanFile(file);
+
+            string[] subdirectoryExists = Directory.GetDirectories(directory);
+            foreach (string subdirectory in subdirectoryExists)
+                ScanDirectory(subdirectory);
+        }
+
+        public static void ScanFile(string path)
+        {
+            string destination = DestinationPath + path.Substring(path.LastIndexOf(@"\"));
+
+            if (path.EndsWith(Filter))
+            {
+                if (!Directory.Exists(DestinationPath))
+                {
+                    Directory.CreateDirectory(DestinationPath);
+                }
+                if (!File.Exists(destination))
+                {
+                    File.Move(path, destination, true);
+                    moved++;
+                }
+            }
+            else
+            {
+                unableToMove++;
+            }
+        }
+
+        public static void ScanComplete()
+        {
+            if (moved > 0)
+            {
+                Console.WriteLine($"Moved {moved} files from {SourcePath} to {DestinationPath}.");
+            }
+            if (unableToMove > 0)
+            {
+                Console.WriteLine($"Unable to move {unableToMove} files from {SourcePath} to {DestinationPath}.");
+            }
+            Console.WriteLine("\n\n\rScan complete...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using GeminiSkyFileMigrationTool;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Start:
+        Console.WriteLine("File Migration Tool\n\n");
+
+        Console.Write("Enter the source path: ");
+        string sourcePath = Console.ReadLine();
+        Console.WriteLine(sourcePath);
+
+        Console.Write("\n\nEnter the destination path: ");
+        string destinationPath = Console.ReadLine();
+        Console.WriteLine(destinationPath);
+
+        Console.Write("\n\nEnter the filter: ");
+        string filter = Console.ReadLine();
+        Console.WriteLine(filter);
+
+        Console.Write($"\n\nMigrate {filter} files from {sourcePath} to {destinationPath}? Y/N: ");
+        string input = Console.ReadLine();
+
+        if (input.ToLower() != "y")
+        {
+            goto Start;
+        }
+        else
+        {
+            new FileMigration(sourcePath, destinationPath, filter);
+
+            Console.WriteLine($"\n{FileMigration.SourcePath}, {FileMigration.DestinationPath}, {FileMigration.Filter}\n\n");
+
+            string[] path = { FileMigration.SourcePath };
+
+            foreach (string file in path)
+            {
+                if (File.Exists(file))
+                {
+                    FileMigration.ScanFile(file);
+                }
+                else if (Directory.Exists(file))
+                {
+                    FileMigration.ScanDirectory(file);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a valid file or directory.", path);
+                }
+            }
+        }
+
+        FileMigration.ScanComplete();
+    }
+}
